332282772f9a9dbcade128831d8410cdd5aa344e
Yamin H <99179344+RobinMillford@users.noreply.github.com>
2023-05-22 22:49:44 +0530

    Update main.py

--- SlackBot - A Slackbot to Calculate User's Age (Python)/main.py
+++ SlackBot - A Slackbot to Calculate User's Age (Python)/main.py
@@ -3,8 +3,8 @@ from slack_sdk import WebClient
 from slack_sdk.rtm import RTMClient
 from datetime import datetime
 
-slack_bot_token = 'xoxb-5314817250849-5326501127008-1WQrCmaI4d5bgqaTjku3mREN'
-slack_app_token = 'xapp-1-A058T88BRNH-5326529475120-7813dbd44a68ba3f81cc1180f73fd9f822765533d661892d53ddbe166edb1d9a'
+slack_bot_token = ''
+slack_app_token = ''
 client = WebClient(token=slack_bot_token)
 
 def calculate_age(input_date):
